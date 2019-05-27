3bd12f3d82881696f5cb758fed05e8a9722b37cd
Kieran <3812864+MDCEY@users.noreply.github.com>
2019-05-27 12:01:14 +0100

    Going for a restructure

--- Rimoto_plex_companion/tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-from Rimoto_plex_companion.tasks.scanner import Scanner
-
-
-scanner_task = Scanner()
-scanner_data = {
-    'id': scanner_task.thread.ident,
-    'is_alive' : scanner_task.thread.is_alive()
-}
